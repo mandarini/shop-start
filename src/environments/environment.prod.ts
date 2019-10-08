24bf10802344c0af46ef3b7e2011531719254904
Katerina Skroumpelou <sk.katherine@gmail.com>
2019-10-08 16:59:59 +0300

    fixed typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyCGTk0pRkwY52zfEMko3PyhjkFfKZwSTkw",
+    authDomain: "my-nice-project-name.firebaseapp.com",
+    databaseURL: "https://my-nice-project-name.firebaseio.com",
+    projectId: "my-nice-project-name",
+    storageBucket: "my-nice-project-name.appspot.com",
+    messagingSenderId: "113069583485",
+    appId: "1:113069583485:web:f7609f061a63949595d514",
+    measurementId: "G-GLG86J45CG"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,14 +10,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyCGTk0pRkwY52zfEMko3PyhjkFfKZwSTkw",
+    authDomain: "my-nice-project-name.firebaseapp.com",
+    databaseURL: "https://my-nice-project-name.firebaseio.com",
+    projectId: "my-nice-project-name",
+    storageBucket: "my-nice-project-name.appspot.com",
+    messagingSenderId: "113069583485",
+    appId: "1:113069583485:web:f7609f061a63949595d514",
+    measurementId: "G-GLG86J45CG"
   }
 };
 
